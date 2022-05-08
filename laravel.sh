df62977e5c7b728bff1e4312067485f4fd3a0fec
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-05-08 16:12:49 +0530

    Update laravel.sh

--- laravel.sh
+++ laravel.sh
@@ -4,10 +4,10 @@ cd /home/ubuntu
 sudo apt update
 sudo apt install apache2 -y
 sudo apt install php -y
-sudo apt install composer -y
+sudo apt install composer 
 curl -s https://getcomposer.org/installer | php    
 mv composer.phar /usr/local/bin/composer  
 composer global require laravel/installer
-#sudo apt install php-xml
+
 sudo apt update
 
